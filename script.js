262a95bec4ec6a03f13c8405d5dd5c3fad219628
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-08-10 20:16:21 +0100

    Remove unneccessary console log

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function createDiv() {
     const container = document.querySelector(".container");
     for(let a = 0; a < 16 * 16; a++) {
-        console.log("Test.");
         const div = document.createElement("div");
         container.appendChild(div);
     }
