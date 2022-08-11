43df5bd4972309a51cb5746711ac78cf4383f87f
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-08-11 20:26:15 +0100

    Modify function to create divs and return them

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ function createDiv() {
         const div = document.createElement("div");
         container.appendChild(div);
     }
+    return container.childNodes;
+}
+
+for (child in container.childNodes) {
+    if (container.childNodes.hasOwnProperty(child)) {
+        console.log(child);
+    }
 }
 
 createDiv();
