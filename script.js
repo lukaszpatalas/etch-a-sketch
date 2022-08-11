b736b9cb5a1fb827b5a62443b30e357fcdb753d0
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-08-11 21:12:32 +0100

    Create testing code to highlight certain div

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 function createDiv() {
     const container = document.querySelector(".container");
-    for(let a = 0; a < 16 * 16; a++) {
+    for (let a = 0; a < 16 * 16; a++) {
         const div = document.createElement("div");
         container.appendChild(div);
     }
+    container.childNodes[3].classList.toggle("hoverColor");
     return container.childNodes;
 }
 
 function start() {
     const createdDivs = createDiv();
+
+    for (let a = 3; a < 10; a++) {
+        if (createdDivs.hasOwnProperty(a)) {
+            console.log(createdDivs[a]);
+        }
+    }
 }
 
+function toggleColor(div) {
+    div.classList.toggle("hoverColor");
+}
 
 /* for (child in container.childNodes) {
     if (container.childNodes.hasOwnProperty(child)) {
@@ -18,4 +28,4 @@ function start() {
     }
 } */
 
-createDiv();
+start();
